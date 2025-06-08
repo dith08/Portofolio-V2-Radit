4048370305d344b9c0b775c06a04bfeb005baaf0
Radithia <141038766+dith08@users.noreply.github.com>
2025-06-09 01:27:57 +0700

    fix: disable eslint in production

--- src/app/components/ui/AboutText.tsx
+++ src/app/components/ui/AboutText.tsx
@@ -2,7 +2,7 @@ const AboutText = () => {
   return (
     <div className="max-w-md text-center md:text-left my-8">
       <p className="text-black text-xl mb-4 font-light">Hello everyone! 👋</p>
-      <p className="text-gray-800 leading-relaxed font-light font-">
+      <div className="text-gray-800 leading-relaxed font-light font-">
         <p>
           I'm Radithia Arlistian Saputra, a Full Stack & Mobile Developer 
         </p>
@@ -16,7 +16,7 @@ const AboutText = () => {
         user-friendly apps, and always eager to grow through
         </p>
         <p className="mt-1"> new challenges solo or in a team. 💻🚀</p>
-      </p>
+      </div>
     </div>
   );
 };
